86d544a25ee493b7d73a2b6024808cd3e5f13e6a
zitherean <155881038+zitherean@users.noreply.github.com>
2025-12-22 23:28:58 +0100

    Add additional button for Player Profile in Home

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -15,25 +15,29 @@ st.subheader("Player Performance")
 
 st.markdown("Use the sidebar or the buttons below to explore different aspects of player performance.")
 
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3, col4, col5, col6 = st.columns(6)
 
 with col1:
+    if st.button("🕸️ Player Profile"):
+        st.switch_page("pages/1_🕸️_Player_Profile.py")
+
+with col2:
     if st.button("🥅 Finishing"):
         st.switch_page("pages/1_🥅_Finishing.py")
 
-with col2:
+with col3:
     if st.button("🎯 Creativity"):
         st.switch_page("pages/2_🎯_Creativity.py")
 
-with col3:
+with col4:
     if st.button("🔁 Build Up Play"):
         st.switch_page("pages/3_🔁_Build_Up_Play.py")
 
-with col4:
+with col5:
     if st.button("📐 Metrics"):
         st.switch_page("pages/4_📐_Metrics.py")
 
-with col5:
+with col6:
     if st.button("🥇 Leaderboard"):
         st.switch_page("pages/5_🥇_Leaderboard.py")
 
